f681c421bbfb09795ad057fa38f7638eba803af7
Pushkar Srivastava <pushkarsrivastava03@gmail.com>
2025-06-06 11:53:44 +0530

    Code Optimization

--- tests/test_login.py
+++ tests/test_login.py
@@ -17,7 +17,8 @@ class TestLogin:
 
      def test_login_success(self):
         self.login_page.enter_username(Config.USERNAME)
-        time.sleep(3)
+        
+
         self.login_page.enter_password(Config.PASSWORD)
         time.sleep(3)
         self.login_page.click_login()
